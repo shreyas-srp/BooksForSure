48247985b036851ca98b86715c911a34daafa1a3
shreyas-srp <shreyas.srp@gmail.com>
2016-01-26 18:03:14 +0530

    edittexts added

---
+++ app/src/main/java/booksforsure/galaxyworks/com/booksforsure/BookListAdapter.java
@@ -0,0 +1,7 @@
+package booksforsure.galaxyworks.com.booksforsure;
+
+/**
+ * Created by shrey on 26-01-2016.
+ */
+public class BookListAdapter {
+}
--- app/src/main/java/booksforsure/galaxyworks/com/booksforsure/MyApp.java
+++ app/src/main/java/booksforsure/galaxyworks/com/booksforsure/MyApp.java
@@ -3,20 +3,31 @@ package booksforsure.galaxyworks.com.booksforsure;
 import android.app.Application;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
+import com.digits.sdk.android.Digits;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by shrey on 26-01-2016.
  */
 public class MyApp extends Application{
+
+    private static final String TWITTER_KEY = "o3BDU8NGDuNtzI4meeG7VSPv6";
+    private static final String TWITTER_SECRET = "tzstIqggBekQHhLacOT8hNq36d6upK9zhCIcQhNGjdD8dMutCG";
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits());
+
         Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this, "Y8JdtwlfkTJqVvGiKkAi2SWFxNlgQyfLiSG6h1EV", "SBwObRS30bOlMSUDADuaciFM5SJLtg6bnJLmMtCw");
+        Parse.initialize(this, "YOLPl8WD6T36lrvNPDKVszMuwpodsYUhxXBKpmoq", "0TVywLlePatIcptbzIVtDWNj5L0tDhE7ZrbWLkjg");
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
         TypefaceProvider.registerDefaultIconSets();
