64e3102440be527f910995b871a61678e9f3dc03
Aqeel <legend.dragonborn@gmail.com>
2016-02-17 20:57:58 +0530

    History to be completed

---
+++ app/src/main/java/booksforsure/galaxyworks/com/booksforsure/ContactUs.java
@@ -0,0 +1,15 @@
+package booksforsure.galaxyworks.com.booksforsure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+    }
+}
